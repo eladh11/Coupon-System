7b74fc76b2f26ea8c9b9141ff630892f7da11656
Elad Hakmon <eladhakmon11@gmail.com>
2021-12-07 13:28:36 +0200

    First Commit

---
+++ cs/server.js
@@ -0,0 +1,19 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/cs'));
+// app.use(express.static('./dist/cs'));
+
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/dist/cs/index.html'));
+});
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/cs/'}),
+// );
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT ||8080);
